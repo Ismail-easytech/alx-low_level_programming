f0e9ad0f8d3700f6868076ad5baeaebac2d4c153
Ismail-easytech <ismaeelazeez.ia@gmail.com>
2023-04-06 03:48:56 -0700

    2 New updated C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strchr - locates a character in a string
  *
@@ -9,19 +8,17 @@
  *
  * Return: a pointer to the first occurrence of the character
 */
-
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-	if (c == '\0')
-		return (s);
-
-	return (NULL);
+while (*s)
+{
+if (*s != c)
+s++;
+else
+return (s);
+}
+if (c == '\0')
+return (s);
+return (NULL);
 }
 
