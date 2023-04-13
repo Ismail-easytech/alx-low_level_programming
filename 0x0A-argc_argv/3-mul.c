3e752c8cb5f82dc3d78bf1fb41a1acdc390993d8
Ismail-easytech <ismaeelazeez.ia@gmail.com>
2023-04-13 06:06:08 -0700

    0x0A-argc_argv mul.c updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,15 @@
 */
 int main(int argc, char **argv)
 {
-	int x, y;
-
-	if (argc < 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	printf("%d\n", x * y);
-
-	return (0);
+int x, y;
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
 }
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x * y);
+return (0);
 }
+
